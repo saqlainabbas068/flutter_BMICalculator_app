ead3f503bb437701528085d9c9f2b3d71caf2d2e
MUHAMMAD SAQLAIN ABBAS <saqlaindogar5@gmail.com>
2020-11-24 17:39:06 +0500

    Done Lecture57 PART 4:Use of Refectored code

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class InputPage extends StatefulWidget {
   @override
@@ -15,56 +16,97 @@ class _InputPageState extends State<InputPage> {
         title: Text('BMI CALCULATOR' ),
       ),
       body: Column(
-        children: <Widget>[
-          Expanded(child: Row(
-            children: <Widget>[
-              Expanded(
-                child: new RepeatContainerCode(
-                  colors: Colors.blue,
-                ),
-              ),
-              Expanded(
-                  child: new RepeatContainerCode(
-                    colors: Color(0xFF1D1E33),
-                  ),
-              ),
-            ],
-          ),
-          ),
+      children: <Widget>[
+        Expanded(child: Row(
+          children: <Widget>[
+            Expanded(
+              child: new RepeatContainerCode(
+                colors: Color(0xFF1D1E33),
+                cardWidget: RepeatTextAndIconWidget(
+                iconData:FontAwesomeIcons.male,
+                label: "MALE",
 
-          Expanded(child: new RepeatContainerCode(
-            colors: Color(0xFF1D1E33),
-          ),
-          ),
-
-          Expanded(child: Row(
-            children: <Widget>[
-            Expanded(child: new RepeatContainerCode(
-              colors: Color(0xFF1D1E33),
-            ),
+              ),
+              ),
             ),
             Expanded(
                 child: new RepeatContainerCode(
                   colors: Color(0xFF1D1E33),
+                  cardWidget: RepeatTextAndIconWidget(
+                  iconData:FontAwesomeIcons.female,
+                  label: "FEMALE",
+    ),
                 ),
             ),
-           ],
-         ),
-          ),
+          ],
+        ),
+        ),
 
+        Expanded(child: new RepeatContainerCode(
+          colors: Color(0xFF1D1E33),
+        ),
+        ),
+
+        Expanded(child: Row(
+          children: <Widget>[
+          Expanded(child: new RepeatContainerCode(
+            colors: Color(0xFF1D1E33),
+          ),
+          ),
+          Expanded(
+              child: new RepeatContainerCode(
+                colors: Color(0xFF1D1E33),
+              ),
+          ),
          ],
-      ),
+       ),
+        ),
+       ],
+    ),);
+  }
+}
+
+class RepeatTextAndIconWidget extends StatelessWidget {
+  RepeatTextAndIconWidget({@required this.iconData, this.label});
+
+  final IconData iconData;
+  final String label;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(
+          iconData,
+          size: 80.0,
+        ),
+        SizedBox(
+          height: 15.0,
+        ),
+        Text(
+          label,
+          style: TextStyle(
+          fontSize: 18.0,
+
+        ),
+        ),
+      ],
     );
   }
 }
 
 class RepeatContainerCode extends StatelessWidget {
-  RepeatContainerCode({@required this.colors});
+  RepeatContainerCode({@required this.colors,this.cardWidget});
   final Color colors;
+  final Widget cardWidget;
+
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(15.0),
+      child:cardWidget,
       decoration: BoxDecoration(
         color: colors,
         borderRadius: BorderRadius.circular(10.0),
